7a75971ff8c7cb87534213886d35c1f61dcc3f85
julioreisdev <juliosteam0352@gmail.com>
2022-06-23 17:05:13 -0300

    Adiciona async await para concertar bug de callback hell

--- src/app.js
+++ src/app.js
@@ -22,41 +22,41 @@ const userSchema = Joi.object({
   name: Joi.string().min(1).required(),
 });
 
-app.get("/participants", (req, res) => {
-  db.collection("users")
-    .find()
-    .toArray()
-    .then((users) => res.send(users));
+app.get("/participants", async (req, res) => {
+  try {
+    const users = await db.collection("users").find().toArray();
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.post("/participants", (req, res) => {
+app.post("/participants", async (req, res) => {
   const { error, values } = userSchema.validate(req.body);
   if (error) {
     res.sendStatus(422);
     return;
   }
-  db.collection("users")
-    .findOne(req.body)
-    .then((user) => {
-      if (user) {
-        res.sendStatus(409);
-      } else {
-        let nome = req.body.name;
-        let hora = Date.now();
-        db.collection("users")
-          .insertOne({ name: nome, lastStatus: hora })
-          .then(() => {
-            db.collection("messages").insertOne({
-              from: nome,
-              to: "Todos",
-              text: "entra na sala...",
-              type: "status",
-              time: dayjs().format("HH:mm:ss"),
-            });
-            res.sendStatus(201);
-          });
-      }
-    });
+  try {
+    const user = await db.collection("users").findOne(req.body);
+    if (user) {
+      res.sendStatus(409);
+    } else {
+      let nome = req.body.name;
+      let hora = Date.now();
+      await db.collection("users").insertOne({ name: nome, lastStatus: hora });
+      await db.collection("messages").insertOne({
+        from: nome,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss"),
+      });
+      res.sendStatus(201);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(5000, () => {
