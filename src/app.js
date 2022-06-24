3efa79c95cb830249dbdb378105b4003b2b4bd1a
julioreisdev <juliosteam0352@gmail.com>
2022-06-24 00:05:19 -0300

    Adiciona rota get para retorno de mensagens

--- src/app.js
+++ src/app.js
@@ -22,6 +22,12 @@ const userSchema = Joi.object({
   name: Joi.string().min(1).required(),
 });
 
+const messageSchema = Joi.object({
+  to: Joi.string().min(1).required(),
+  text: Joi.string().min(1).required(),
+  type: Joi.string().valid("message", "private_message").required(),
+});
+
 app.get("/participants", async (req, res) => {
   try {
     const users = await db.collection("users").find().toArray();
@@ -60,6 +66,71 @@ app.post("/participants", async (req, res) => {
   } catch (error) {
     res.send(error).status(500);
   }
+});
+
+app.post("/messages", async (req, res) => {
+  const { error, values } = messageSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  const from = req.headers.user;
+  if (error) {
+    const detalhes = error.details.map((detail) => detail.message);
+    res.status(422).send(detalhes);
+    return;
+  }
+  const user = await db.collection("users").findOne({ name: from });
+  if (!user) {
+    res.status(422).send("Invalid user!");
+    return;
+  }
+  await db.collection("messages").insertOne({
+    to: req.body.to,
+    text: req.body.text,
+    type: req.body.type,
+    from: from,
+    time: dayjs().format("HH:mm:ss"),
+  });
+  res.sendStatus(201);
+});
+
+app.get("/messages", async (req, res) => {
+  const limite = req.query.limit;
+  const user = req.headers.user;
+  let mensagens;
+  mensagens = await db
+    .collection("messages")
+    .find()
+    .sort({ time: -1 })
+    .toArray();
+  const mensagensValidas = mensagens.filter((mensagen) => {
+    if (
+      mensagen.to === "Todos" ||
+      mensagen.to === user ||
+      mensagen.from === user
+    ) {
+      return mensagen;
+    }
+    return false;
+  });
+  let totalMensagensVisiveis = [];
+  if (limite) {
+    let limit = parseInt(limite);
+    for (let i = 0; i < limit; i++) {
+      totalMensagensVisiveis.push(mensagensValidas[i]);
+      if (i === mensagensValidas.length - 1) {
+        break;
+      }
+    }
+  } else {
+    let limit = 100;
+    for (let i = 0; i < limit; i++) {
+      totalMensagensVisiveis.push(mensagensValidas[i]);
+      if (i === mensagensValidas.length - 1) {
+        break;
+      }
+    }
+  }
+  res.send(totalMensagensVisiveis);
 });
 
 app.listen(5000, () => {
